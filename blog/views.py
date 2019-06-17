4e1f1ffe2e20b950954f0cdd2599e971a2f30c3d
NicolasDutour <dutourn99@gmail.com>
2019-06-17 17:48:38 +0200

    Create post and comment models and views

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
+from .models import Post
 
 
 def blog_index(request):
-    return render(request, 'blog/blog_index.html')
+    posts = Post.objects.all()
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/blog_index.html', context)
