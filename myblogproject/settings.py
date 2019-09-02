fea91fed3028276aed78038e7fe3a3beb5d6f759
NicolasDutour <dutourn99@gmail.com>
2019-09-02 16:23:53 +0200

    add postgresqlq DB

--- myblogproject/settings.py
+++ myblogproject/settings.py
@@ -74,13 +74,24 @@ WSGI_APPLICATION = 'myblogproject.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+if DEBUG:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
     }
-}
-
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': 'myblogprojectDB',
+            'USER': 'blog_admin',
+            'PASSWORD': 'Kitesurf1983',
+            'HOST': 'localhost',
+            'PORT': '',
+        }
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
