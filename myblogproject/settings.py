1acc5bba9463fff3e5fbd4bd154fbfec5b6ba3af
NicolasDutour <dutourn99@gmail.com>
2019-09-02 16:24:51 +0200

    change DEBUG to False

--- myblogproject/settings.py
+++ myblogproject/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '@3)lml1)gt#hl7m07%i7u$&1x1r)y$pc)y4fx_$mmom+!0@wd2'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['165.227.87.161']
 
